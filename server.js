1bbd29c5112ae7b872f2dc63514bb6daa2c6fd19
Michael Johnston <mjjohnston55@gmail.com>
2019-10-19 11:41:05 -0700

    route names added FO REAL

--- server.js
+++ server.js
@@ -28,4 +28,17 @@ function Reservation(name, phoneNumber, email, id) {
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
+
+app.get("/", function(req, res) {
+res.send("Welcome! Make a new reservation.");
+});
+
+app.get("/tables", function(req, res) {
+res.sendFile(path.join(__dirname, ""));
+});
+
+app.get("/reserve", function(req, res) {
+res.sendFile(path.join(__dirname, ""));
+});
+  
   
