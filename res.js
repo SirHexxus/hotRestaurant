4c5e4e512417f1e8d595b5864fbbb17ee626f8f6
Devyn Barron <52061626+barrd001@users.noreply.github.com>
2019-10-19 13:30:11 -0700

    added ext js file for res page

--- res.js
+++ res.js
@@ -1,3 +1,20 @@
 // Capture form input
+$('.submit').on('click', function(event) {
+  event.preventDefault();
 
-// Push form info into server
+  let res = {
+    customerName: $('#name').val().trim(),
+    customerPhone: $('#phone').val().trim(),
+    customerEmail: $('#email').val().trim(),
+    customerID: $('#id').val().trim()
+  };
+  console.log(res);
+
+  // $.post()
+
+
+  $('#name').val('');
+  $('#phone').val('');
+  $('#email').val('');
+  $('#id').val('');
+});
